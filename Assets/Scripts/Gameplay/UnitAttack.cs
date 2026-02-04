35b3f344e9bafbc757b521d473a49ccf4e1ec90a
Pellelapell <timdem@outlook.fr>
2026-02-04 01:47:18 +0100

    Attack begin

---
+++ Assets/Scripts/Gameplay/UnitAttack.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAttack : MonoBehaviour
+{
+    int unitAttackDamage;
+    float unitAttackSpeed;
+    float unitAttackRange;
+    List<GameObject> targetList;
+
+    void Awake()
+    {
+        targetList = new List<GameObject>();
+        UnitStats stats = GetComponent<UnitStats>();
+        unitAttackDamage = stats.attackDamage;
+        unitAttackSpeed = stats.attackSpeed;
+        unitAttackRange = stats.attackRange;
+    }
+
+    void Update()
+    {
+        GetTargets();
+    }
+
+
+    GameObject GetTarget()
+    {
+
+    }
+}
