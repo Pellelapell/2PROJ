5069feeabb8fc0603dad357d11984a95fa03cbe7
Heaguke <tom.petitprez.nsi@gmail.com>
2026-04-16 17:22:23 +0200

    modiff
    fff

--- Assets/Scripts/Gameplay/Units/UnitVisuals.cs
+++ Assets/Scripts/Gameplay/Units/UnitVisuals.cs
@@ -5,13 +5,33 @@ public class UnitVisuals : MonoBehaviour
 {
     [Header("Visual Components")]
     private Renderer unitRenderer;
-
     private UnitStats stats;
+
+    [Header("Health Bar")]
+    [SerializeField] private GameObject healthBarPrefab;
+    private HealthBar healthBar;
 
     private void Awake()
     {
         stats = GetComponent<UnitStats>();
         unitRenderer = GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        if (healthBarPrefab != null)
+        {
+            GameObject hbObj = Instantiate(healthBarPrefab);
+            healthBar = hbObj.GetComponent<HealthBar>();
+            healthBar.Initialize(stats);
+            stats.OnHealthChanged += healthBar.UpdateHealth;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (stats != null && healthBar != null)
+            stats.OnHealthChanged -= healthBar.UpdateHealth;
     }
 
     public void ApplyRaceVisuals()
